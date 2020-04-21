67913a9da6b3331605767d637e456c7d860747a4
v.mager <v.mager@b2b-center.ru>
2020-04-21 20:01:40 +0300

    burning jude

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Hello git');
+
+class Reich {
+  constructor(fuhrer) {
+    this.fuhrer = fuhrer;
+  }
+
+  burningJude() {
+    console.log(this.fuhrer + ' burning one jude');
+  }
+}
+
+const reich = new Reich('Adolf Hilter');
+reich.burningJude();
